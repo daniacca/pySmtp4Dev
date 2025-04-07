2f420dbc0af402cf237133019cffd8c232c79e53
daniacca <d.acca87@gmail.com>
2025-04-07 14:13:15 +0200

    fix: enabled tls

--- smtp_send_data.py
+++ smtp_send_data.py
@@ -1,11 +1,10 @@
 import smtplib
-from smtp_utils import ssl_certificate_helper
 from email.mime.text import MIMEText
 
 # Create the message
 msg = MIMEText('This is the body of the message.')
 me = 'Jon@MacBookPro.com'
-you = 'jon.drowell@yahoo.com'
+you = 'jon.drowell@yahoo'
 
 msg['Subject'] = 'The contents of the log file'
 msg['From'] = me
@@ -13,7 +12,8 @@ msg['To'] = you
 
 server = smtplib.SMTP('localhost', 2525)
 server.set_debuglevel(True) # show communication with the server
-#server.starttls()
+server.starttls()
+
 try:
     server.sendmail(me, [you], msg.as_string())
 finally:
